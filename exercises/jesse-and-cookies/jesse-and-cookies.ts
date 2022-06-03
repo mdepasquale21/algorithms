af9f90a9f23a7fab2dba29575fb519323fe59e8f
mdepasquale <depasquale@esteco.com>
2022-06-03 19:17:30 +0200

    minor improvements to jesse and cookies

--- exercises/jesse-and-cookies/jesse-and-cookies.ts
+++ exercises/jesse-and-cookies/jesse-and-cookies.ts
@@ -9,30 +9,22 @@ console.log(`k: ${k}`);
 console.log(`sweetness array: ${array}`);
 console.log('\n');
 
-array.sort((a, b) => a - b);
-
-console.log('sorted array:');
-console.log(array);
-console.log('\n');
-
 let counter = 0;
 while (isSomeNumberSmallerThan(array, k)) {
+    array.sort((a, b) => a - b);
+    console.log(array);
     const leastTwo = array.splice(0, 2);
     console.log(leastTwo);
     const newSweetness = leastTwo[0] + 2 * leastTwo[1];
     console.log(newSweetness);
     array.push(newSweetness);
-    array.sort((a, b) => a - b);
-    console.log(array);
     counter += 1;
 }
 console.log('\n');
+console.log('final cookies:');
+console.log(array);
 console.log('final number of operations required:');
 console.log(counter);
-
-function isEachNumberLargerThan(list: number[], k: number) {
-    return list.every((n: number) => n >= k);
-}
 
 function isSomeNumberSmallerThan(list: number[], k: number) {
     return list.some((n: number) => n < k);
